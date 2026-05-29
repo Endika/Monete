541b2d4b991f05e04a33c5a4ca1c911986d32f0a
Endika Iglesias <endika2@gmail.com>
2026-05-29 22:22:01 +0200

    feat(util): google maps search url

---
+++ tests/shared/googleMapsUrl.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import { googleMapsUrl } from '@/shared/utils/googleMapsUrl'
+
+describe('googleMapsUrl', () => {
+  it('builds an encoded maps search url', () => {
+    expect(googleMapsUrl('Fun Park 12, Madrid')).toBe('https://www.google.com/maps/search/?api=1&query=Fun%20Park%2012%2C%20Madrid')
+  })
+})
